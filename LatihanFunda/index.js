2baeebc4497fa2df8cfd70baa816e04873cbae5d
bombomreza <mrezas1616@gmail.com>
2020-11-25 14:08:52 +0700

    React JS

---
+++ LatihanFunda/index.js
@@ -0,0 +1,21 @@
+// function addArr(arr1, arr2){
+//     var num = 0;
+//     for (let i = 0; i < arr1.length; i++) {
+//         num += arr1[i] + arr2[i]
+//     }
+//     // console.log(num)
+// }
+// addArr([1, 2, 3], [4, 5, 6])
+// // console.log(addArr[1,2,3],[4,5,6])
+
+function genap(num){
+    var output = ""
+    var a = 1
+    for (let i = 0; i < num ; i++) {
+        if (a % 2 === 0)
+        output = 
+        a++
+    }
+    return output
+}
+console.log(genap(20))
