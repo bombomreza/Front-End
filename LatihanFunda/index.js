cb4554d8420dcdf00be09d32598f1236a93523f3
bombomreza <mrezas1616@gmail.com>
2020-11-26 19:40:32 +0700

    Latihan React Js

--- LatihanFunda/index.js
+++ LatihanFunda/index.js
@@ -8,14 +8,52 @@
 // addArr([1, 2, 3], [4, 5, 6])
 // // console.log(addArr[1,2,3],[4,5,6])
 
-function genap(num){
-    var output = ""
-    var a = 1
-    for (let i = 0; i < num ; i++) {
-        if (a % 2 === 0)
-        output = 
-        a++
-    }
-    return output
-}
-console.log(genap(20))
+// function genap(num){
+//     var output = ""
+//     var a = 1
+//     for (let i = 0; i <= num ; i++) {
+//         i !== num ? (output+= `${i}`) : ()
+//     }
+//     return output
+// }
+// console.log(genap(20))
+
+// function addArr(arr1, arr2){
+//     var num = [];
+//     var num1 = "";
+//     var num2 = "";
+    
+//     for (let i = 0; i< arr1.length; i++) {
+//         num[i] = arr1[i] + arr2[i]
+//         num2 = arr1[i] + arr1[i] 
+//     }
+//     return num
+// }
+// console.log(addArr([1,2,3], [4,5,6]))
+// console.log(addArr([4,5,6], [7,8,9]))
+// console.log(addArr([4,5,6,7,8], [7,8,9,10,11]))
+
+// function changeEven(arr){
+//     var nama = arr.split("")
+//     var change = ""
+    
+//     for (let i = 0; i < nama.length; i++) {
+//         if (i % 2 == 0){
+            
+//         }
+//     }
+//     return nama.length
+// }
+// console.log(changeEven('Bubu'))
+
+// function multiply(arr){
+//     var num = ""
+//     var a = 1
+//     for (let i = 0; i < arr; i++) {
+//         num = num * a
+//         a++
+//     }
+//     return a
+// }
+// console.log(multiply(5))
+
